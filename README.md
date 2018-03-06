aded0a4a052db2c6074a60ea5f716dae1d0782e6
lei <oizhaolei@gmail.com>
2018-03-06 14:42:20 +0800

    接口测试

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ $ npm install --save oizhaolei/node-das-ecard
 
 
 ## Usage
-
 ```js
 var Ecard = require('node-das-ecard');
 var ecard = new Ecard({
@@ -28,7 +27,7 @@ const data = await ecard.fetch('func', {
 ```sh
 npm install
 export PUBLIC_KEY=DUMMY_PUBLIC_KEY_QEBAQUAAAAAAAAAAAAAAAAlgGyIXmGHfbxSkWlG1OrMjKErNS2vq4Q1Ay/o3ne2sxcoITze5sShffHAOFbWK2YGz1c9MJVkw2YPkLGzQbwxkGi+1O5g4MiPAd6GS7GJhalDpip3Qu7arMpOZ6CUaxW8BB/OvaE1U6y7JbPsMlnQnJqSLQySiXq3A8XOzYPEwIDAQAB &&
-    export IV=17,2,3,6,5,8,7,2 &&
+    export IV=1,2,3,4,5,6,7,8 &&
     export URL=http://192.168.24.1:80 &&
     export REDIS_URL=redis://localhost:6379
 mocha test/index.js
--- test/index.js
+++ test/index.js
@@ -9,25 +9,202 @@ const ecard = new Ecard({
 });
 
 describe('module', () => {
-  // 用户注册
-  it('UserRegister', async () => {
-    const MethodName = 'UserRegister';
+//   // 用户注册
+//   it('UserRegister', async () => {
+//     const MethodName = 'UserRegister';
+//     // const PhoneNO = '15841165915';
+//     const PhoneNO = '18810981100';
+//     // const PhoneNO = '13284117088';
+
+//     const reg = await ecard.fetch({
+//       MethodName: 'UserRegister',
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // Token注册
+//   it('RegisterSystem', async () => {
+//     const MethodName = 'RegisterSystem';
+//     const PhoneNO = '18810981100';
+
+//     const reg = await ecard.fetch({
+//       MethodName: 'RegisterSystem',
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 充值
+//   it('PutMoney', async () => {
+//     const MethodName = 'PutMoney';
+//     const PhoneNO = '18810981100';
+//     const PutKind = 'APP帐号'; //微信、支付宝、APP帐号
+//     const PutValue = '152';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//       PutKind,
+//       PutValue,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 挂失
+//   it('ExecuteCardLoss', async () => {
+//     const MethodName = 'ExecuteCardLoss';
+//     const PhoneNO = '18810981100';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 解挂
+//   it('ExecuteCardCancelLoss', async () => {
+//     const MethodName = 'ExecuteCardCancelLoss';
+//     const PhoneNO = '18810981100';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 充值查询
+//   it('QueryPutMoneyData', async () => {
+//     const MethodName = 'QueryPutMoneyData';
+//     const PhoneNO = '18810981100';
+//     const BeginTime = '2018-3-1 13:00';
+//     const EndTime = '2018-3-1 18:00';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//       BeginTime,
+//       EndTime,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 消费查询
+//   it('QueryXFData', async () => {
+//     const MethodName = 'QueryXFData';
+//     const PhoneNO = '18810981100';
+//     const BeginTime = '2018-2-27 13:00';
+//     const EndTime = '2018-2-27 15:00';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//       BeginTime,
+//       EndTime,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 记录查询（挂失，解挂，补卡）
+//   it('QueryCardLossData', async () => {
+//     const MethodName = 'QueryCardLossData';
+//     const PhoneNO = '18810981100';
+//     const BeginTime = '2018-2-27 13:00';
+//     const EndTime = '2018-3-1 18:00';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//       BeginTime,
+//       EndTime,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 一卡通余额查询
+//   it('QueryCardMoney', async () => {
+//     const MethodName = 'QueryCardMoney';
+//     const PhoneNO = '18810981100';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 二维码开门
+//   it('GetQrCode', async () => {
+//     const MethodName = 'GetQrCode';
+//     const PhoneNO = '18810981100';
+//     const QrcodeType = '0';    //二维码类型 0 员工 1访客
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       QrcodeType,
+//       PhoneNO,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+// describe('module', () => {
+//   // 访客邀请
+//   it('GetInviteData', async () => {
+//     const MethodName = 'GetInviteData';
+//     const PhoneNO = '18810981100';
+//     const VstName = '小王';
+//     const VstPhone = '13915468546';
+//     const VstIDNo = '210202154684684548';
+//     const VstBeginTime = '2018-2-27 13:00';
+//     const VstHour = '3';
+//     const RsnDesc = '商务访谈';
+//     const VstRegion = '0';
+
+//     const reg = await ecard.fetch({
+//       MethodName,
+//       PhoneNO,
+//       VstName,
+//       VstPhone,
+//       VstIDNo,
+//       VstBeginTime,
+//       VstHour,
+//       RsnDesc,
+//       VstRegion,
+//     });
+//     console.log('reg', reg);
+//   });
+// });
+
+//  个人资料查询
+  it('GetPersonalInfo', async () => {
+    const MethodName = 'GetPersonalInfo';
     const PhoneNO = '18810981100';
 
     const reg = await ecard.fetch({
-      MethodName: 'UserRegister',
+      MethodName,
       PhoneNO,
     });
     console.log('reg', reg);
   });
-  // Token值注册
-  /* it('RegisterSystem', async () => {
-   *   const PhoneNO = '18810981100';
-
-   *   const reg = await ecard.fetch('RegisterSystem', {
-   *     PhoneNO,
-   *   });
-   *   console.log('reg', reg);
-   *   return;
-   * });*/
 });
